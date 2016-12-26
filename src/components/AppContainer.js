a6344a6b3321f3edd01d5f04636d70874fc01b75
nekrasov-iv <nekrasov-iv@yandex.ru>
2016-12-26 12:16:47 +0300

    Добавил компонент выбора диапазона дат

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,11 +1,13 @@
 import React, { PropTypes } from 'react'
 import ArticleList from './ArticleList'
 import UserForm from './UserForm'
+import CalendarForm from './CalendarForm'
 import Filter from './Filter'
 
 function AppContainer(props) {
     return (
         <div>
+            <CalendarForm />
             <UserForm />
             <Filter articles = {props.articles}/>
             <ArticleList articles={props.articles}/>
